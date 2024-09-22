9053912724eb82cb694b1dba1030aa53a0177fda
IanSamuelTrujilloGil <iansamueltrujillo@@gmail.com>
2024-09-22 19:56:02 +0100

    Add Person and Main classes.

---
+++ src/main/java/software/ulpgc/Main.java
@@ -0,0 +1,10 @@
+package software.ulpgc;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Person("Ian", LocalDate.of(2002,1,5)));
+
+    }
+}
